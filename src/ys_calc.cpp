6f871201c1472361c4f14930d3c24de9419bb125
jaydogmc <jaydogmc>
2004-02-17 16:08:21 +0000

    The decimal point function kinda works, still working on it

--- src/ys_calc.cpp
+++ src/ys_calc.cpp
@@ -5,12 +5,14 @@
 
 using namespace std;
 
+
+//when a button is pressed, it's ID matches up here with a function
 BEGIN_EVENT_TABLE(YardCalc, wxPanel)
     EVT_BUTTON(ID_CALC_PLUS, YardCalc::OnPlus)
     EVT_BUTTON(ID_CALC_EQUALS, YardCalc::OnEqual)
     EVT_BUTTON(ID_CALC_CLEAR, YardCalc::OnClear)
     EVT_BUTTON(ID_CALC_AC, YardCalc::OnAllClear)
-    EVT_BUTTON(ID_CALC_DOT, YardCalc::OnDot)
+    EVT_BUTTON(ID_CALC_DOT, YardCalc::OnNumber)
     EVT_BUTTON(-1, YardCalc::OnNumber)
 END_EVENT_TABLE()
 
@@ -26,14 +28,16 @@ YardCalc::YardCalc(wxWindow* parent, wxWindowID id,
     sizer->SetSizeHints(this);
     SetSize(sizer->GetMinSize());
     
+    //initialize a way to get to the screen
     m_screen = (wxTextCtrl *)FindWindow(ID_CALC_SCREEN);
+    
+    //make sure that the pointer is active
     wxASSERT(m_screen);
-
+    
+    //initialize the screen
     stringstream num;
     num << m_number;
-    
     m_screen->SetValue(num.str().c_str());
-    
 }
 
 YardCalc::~YardCalc()
@@ -44,7 +48,8 @@ YardCalc::~YardCalc()
 void YardCalc::OnNumber(wxCommandEvent & event)
 {
     wxChar ch;
-    switch (event.GetId()) {
+
+	switch (event.GetId()) {
         case (ID_CALC_1): ch = '1'; break;
         case (ID_CALC_2): ch = '2'; break;
         case (ID_CALC_3): ch = '3'; break;
@@ -64,8 +69,15 @@ void YardCalc::OnNumber(wxCommandEvent & event)
         m_screen->SetValue(ch);
     else
     	// set the value of the screen to origional concatonated with new char
-        m_screen->SetValue(m_screen->GetValue() + ch);
-    
+
+	// if the current char is a ., we only want to add it to the screen
+	// if there is not already one there
+	// but if it isnt a dot dont worry about it and add it to the screen
+	if (ch == '.'){
+		if (! m_screen->GetValue().Contains(".") )	
+			m_screen->SetValue(m_screen->GetValue() + ch);
+	}else
+		m_screen->SetValue(m_screen->GetValue() + ch);
 }
 
 void YardCalc::OnPlus(wxCommandEvent & event)
@@ -91,8 +103,9 @@ void YardCalc::OnPlus(wxCommandEvent & event)
         
 }
 
-void YardCalc::OnClear(wxCommandEvent & event){ 
-
+void YardCalc::OnClear(wxCommandEvent & event){
+	
+	m_screen->SetValue("0");
 }
 
 void YardCalc::OnAllClear(wxCommandEvent & event){
@@ -106,6 +119,7 @@ void YardCalc::OnDot(wxCommandEvent & event){
 void YardCalc::OnEqual(wxCommandEvent & event)
 {
     long int tmp = 0;
+    
     if (m_screen->GetValue().ToLong(&tmp))
         m_number += tmp;
     else {
@@ -116,9 +130,12 @@ void YardCalc::OnEqual(wxCommandEvent & event)
     
     if (m_inAdd) {
         m_number += tmp;
-        stringstream num;
+
+		//write back to the screen
+		stringstream num;
         num << m_number;
         m_screen->SetValue(num.str().c_str());
-        m_inAdd = false;
+       
+	   	m_inAdd = false;
     }
 }
