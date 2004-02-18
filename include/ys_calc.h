c5e8561ab92dcd84d5a1c0086fb135ac30c10bc1
jaydogmc <jaydogmc>
2004-02-18 03:43:09 +0000

    Changes

--- include/ys_calc.h
+++ include/ys_calc.h
@@ -32,6 +32,7 @@ class YardCalc: public wxPanel {
     
     void OnNumber(wxCommandEvent & event);
     void OnPlus(wxCommandEvent & event);
+    void OnMinus(wxCommandEvent & event);
     void OnClear(wxCommandEvent & event);
     void OnAllClear(wxCommandEvent & event);
     void OnDot(wxCommandEvent & event);
