bf3780e07dda8486b9cb67268c29eb724db89148
jaydogmc <jaydogmc>
2004-02-18 02:59:35 +0000

    Removed splash and login screens for convenience

--- src/yardsale.cpp
+++ src/yardsale.cpp
@@ -8,26 +8,26 @@ IMPLEMENT_APP(YardSale)
 
 bool YardSale::OnInit()
 {
-    wxDialog * startup = new YardSplash(NULL, -1, "YardSale");
+//    wxDialog * startup = new YardSplash(NULL, -1, "YardSale");
     
-    startup->ShowModal();
+ //   startup->ShowModal();
     // load here!
-    wxDialog * login = new YardLogin(NULL, -1, "YardSale");
+  //  wxDialog * login = new YardLogin(NULL, -1, "YardSale");
     wxFrame * frame = new YardMain(NULL, -1, "YardSale");
     
-    SetTopWindow(login);
+   // SetTopWindow(login);
     // end loading
-    startup->Destroy();
+    /*startup->Destroy();
     if (login->ShowModal() == 1)
     {
         frame->Destroy();
         login->Destroy();
         return false;
-    }        
+    }        */
     
     SetTopWindow(frame);
     
-    login->Destroy();
+    //login->Destroy();
     
     frame->Show(true);
     
