330775fd14ed11a386f8d030d6aacd6c3898b7dd
Jesse Lovelace <jesse.lovelace@gmail.com>
2004-02-24 22:47:47 +0000

    Adding debug header.

---
+++ include/ys_debug.h
@@ -0,0 +1,18 @@
+#ifndef YS_DEBUG_SCREEN_H
+#define YS_DEBUG_SCREEN_H
+
+#include "wx/frame.h"
+
+class YardDebugScreen: public wxFrame {
+    public:
+        
+    YardDebugScreen(wxWindow* parent, wxWindowID id, const wxString& title,
+               const wxPoint& pos = wxDefaultPosition, 
+               const wxSize& size = wxDefaultSize,
+               long style = wxRESIZE_BORDER|wxFRAME_NO_TASKBAR);
+        
+    virtual ~YardDebugScreen();
+    
+};
+
+#endif
