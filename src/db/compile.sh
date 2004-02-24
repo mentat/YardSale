31483d66ed81c77570df276424c5ab6af7732552
pwizard <pwizard>
2004-02-24 00:42:57 +0000

    and now with Debugging!

--- src/db/compile.sh
+++ src/db/compile.sh
@@ -1,1 +1,1 @@
-gcc `wx-config --cxxflags --libs` -o clidbtest -I../../include clidbtest.cpp ../ys_database.cpp ../ys_inv_type.cpp
+gcc `wx-config --cxxflags --libs` -g -o clidbtest -I../../include clidbtest.cpp ../ys_database.cpp ../ys_inv_type.cpp
