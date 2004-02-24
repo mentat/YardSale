83a3814a120f1fa8dd1c97bae801f176e96718fa
Jesse Lovelace <jesse.lovelace@gmail.com>
2004-02-24 01:41:57 +0000

    Removing pointer.

--- src/ys_database.cpp
+++ src/ys_database.cpp
@@ -8,7 +8,6 @@ YardDatabase::YardDatabase(const wxString& dsn, const wxString& name, const wxSt
 
     m_db = 0;
     m_table = 0;
-    m_inv = 0;
     m_connectInfo = 0;
     Init(dsn, name, pass);
 }
@@ -16,13 +15,11 @@ YardDatabase::YardDatabase(const wxString& dsn, const wxString& name, const wxSt
 YardDatabase::YardDatabase() {
     m_db = 0;
     m_table = 0;
-    m_inv = 0;
     m_connectInfo = 0;
 }
 
 YardDatabase::~YardDatabase() 
 {
-    delete m_inv;
     delete m_connectInfo;
     delete m_db;
     delete m_table;
@@ -30,10 +27,6 @@ YardDatabase::~YardDatabase()
 
 bool YardDatabase::Init(const wxString& dsn, const wxString& name, const wxString& pass)
 {
-    if (m_inv)
-        delete m_inv;
-    
-    m_inv = new YardInvType();
     
     if (m_connectInfo)
         delete m_connectInfo;
@@ -76,25 +69,25 @@ bool YardDatabase::connect(){
 	 SQL_C_TIMESTAMP    SQL_C_BOOLEAN
 	 SQL_C_ENUM
     */  
-    m_table->SetColDefs(0, "INV_SKU_Number", DB_DATA_TYPE_INTEGER, &(m_inv->m_skuNumber), 
-	       SQL_C_ULONG, sizeof(m_inv->m_skuNumber), TRUE, TRUE);
-    m_table->SetColDefs(1, "INV_Bar_Code_Number", DB_DATA_TYPE_VARCHAR, &(m_inv->m_barCode),
-	       SQL_C_CHAR, sizeof(m_inv->m_barCode), TRUE, TRUE);
-    m_table->SetColDefs(2, "INV_Item_Type", DB_DATA_TYPE_VARCHAR, &(m_inv->m_itemType),
-	       SQL_C_CHAR, sizeof(m_inv->m_itemType), TRUE, TRUE);
+    m_table->SetColDefs(0, "INV_SKU_Number", DB_DATA_TYPE_INTEGER, &(m_inv.m_skuNumber), 
+	       SQL_C_ULONG, sizeof(m_inv.m_skuNumber), TRUE, TRUE);
+    m_table->SetColDefs(1, "INV_Bar_Code_Number", DB_DATA_TYPE_VARCHAR, &(m_inv.m_barCode),
+	       SQL_C_CHAR, sizeof(m_inv.m_barCode), TRUE, TRUE);
+    m_table->SetColDefs(2, "INV_Item_Type", DB_DATA_TYPE_VARCHAR, &(m_inv.m_itemType),
+	       SQL_C_CHAR, sizeof(m_inv.m_itemType), TRUE, TRUE);
     /* ... *JOHN*/
-    /*
+    /**
        @todo We need to set up columns for every actual column
        in the table if we plan on using them columns.
     */
     /*   skipping stuff here for a bit.
     */
-    m_table->SetColDefs(3, "INV_Retail_Price", DB_DATA_TYPE_FLOAT, &(m_inv->m_retailPrice),
-	       SQL_C_FLOAT, sizeof(m_inv->m_retailPrice), TRUE, TRUE);
-    m_table->SetColDefs(4, "INV_Item_Description", DB_DATA_TYPE_VARCHAR, &(m_inv->m_itemDescription),
-	       SQL_C_CHAR, sizeof(m_inv->m_itemDescription), TRUE, TRUE);
-    m_table->SetColDefs(5, "INV_Wholesale_Price", DB_DATA_TYPE_FLOAT, &(m_inv->m_wholesalePrice),
-	       SQL_C_FLOAT, sizeof(m_inv->m_wholesalePrice), TRUE, TRUE);
+    m_table->SetColDefs(3, "INV_Retail_Price", DB_DATA_TYPE_FLOAT, &(m_inv.m_retailPrice),
+	       SQL_C_FLOAT, sizeof(m_inv.m_retailPrice), TRUE, TRUE);
+    m_table->SetColDefs(4, "INV_Item_Description", DB_DATA_TYPE_VARCHAR, &(m_inv.m_itemDescription),
+	       SQL_C_CHAR, sizeof(m_inv.m_itemDescription), TRUE, TRUE);
+    m_table->SetColDefs(5, "INV_Wholesale_Price", DB_DATA_TYPE_FLOAT, &(m_inv.m_wholesalePrice),
+	       SQL_C_FLOAT, sizeof(m_inv.m_wholesalePrice), TRUE, TRUE);
     m_table->Open();
     return true;
 }
@@ -111,7 +104,7 @@ vector<YardInvType> YardDatabase::InvSearchKeyword(const unsigned long &sku) {
         return m_invVec;
     
     while (m_table->GetNext()) {
-        m_invVec.push_back(*m_inv);
+        m_invVec.push_back(m_inv);
     }
     return m_invVec;
 }
@@ -143,10 +136,10 @@ vector<YardInvType> YardDatabase::InvGet(unsigned int num, unsigned int offset){
     }
     
     for(int ii = 1; ii < num; ii++){
-        m_invVec.push_back(*m_inv);
+        m_invVec.push_back(m_inv);
         m_table->GetNext();
     }
     
-    m_invVec.push_back(*m_inv);
+    m_invVec.push_back(m_inv);
     return m_invVec;
 }
