906c2213e71ab13f8112d2bfaff7c993ef58a836
pwizard <pwizard>
2004-02-24 05:52:52 +0000

    URRRRRGH

--- src/db/ys_test.cpp
+++ src/db/ys_test.cpp
@@ -10,7 +10,7 @@ int main(){
     otl_long_string test;
     otl_connect db; // connect object
     otl_connect::otl_initialize(); // init ODBC
-    db.rlogon("UID=pwizard;PWD=temp;DSN=YardSale"); // connect to ODBC
+    db.rlogon("UID=REPLACE;PWD=REPLACE;DSN=REPLACE"); // connect to ODBC
     otl_stream dbStream(50, "SELECT INV_SKU_Number, INV_Item_Description "
                         "FROM Inventory_Table WHERE INV_SKU_Number = '10000'",
 			db);
