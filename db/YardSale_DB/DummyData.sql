349db9b3a7704ed1142158ad9624ad6b544e8044
enginerd <enginerd>
2004-02-26 03:09:19 +0000

    I love foreign key constraints

--- db/YardSale_DB/DummyData.sql
+++ db/YardSale_DB/DummyData.sql
@@ -29,7 +29,7 @@ values(2003,'Jesse','L','Lovelace',
 'Jesse  L Lovelace','','' );
 
 INSERT INTO Customer_Table 
-values(2004,'Mike','R','Swigon',
+values(2005,'Mike','R','Swigon',
 'Behind Domino\'s Pizza','919.233.2877',
 'Raleigh','27604','0123-4567-8910-1454', '04/04',
 'Mike R Swigon','','' );
@@ -41,7 +41,7 @@ values(2004,'Jay','','Johnston',
 'Jay Johnston','','' );
 
 INSERT INTO Customer_Table 
-values(2004,'John','','Lamb',
+values(2006,'John','','Lamb',
 'Above Jesse and Jay','919.233.2877',
 'Raleigh','27604','1123-4567-8910-1454', '04/04',
 'John Lamb','','' );
@@ -52,20 +52,20 @@ values(2004,'John','','Lamb',
 
 INSERT INTO Inventory_Table
 values(10000,'1011011010101','Canned Smushed Tomatoes',
-'Food',50,200,35,200,'Can food','1','1',0.80,0.20,
+'Food',50,200,35,200,'Can food',1,1,0.80,0.20,
 '<bulk><amount>10</amount><discount>10</discount></bulk>',
-now(),0.004,'F','F');
+now(),0.004,'F','F','');
 
 INSERT INTO Inventory_Table
 values(10100,'10101101011010101','Hamburger Meat',
-'Food',117,100,50,100,'Meat','1','3',1.99,0.99,
+'Food',117,100,50,100,'Meat',1,3,1.99,0.99,
 '<bulk><amount>5</amount><discount>10</discount></bulk>',
-now(),1.000,'F','F');
+now(),1.000,'F','F','');
 
 INSERT INTO Inventory_Table
 values(10110,'1001101011010101','50 Gallon Trash Can',
-'Houseware',13,0,2,20,'Home','2','2',8.99,3.50,
-'',now(),1.000,'F','F');
+'Houseware',13,0,2,20,'Home',2,2,8.99,3.50,
+'',now(),1.000,'F','F','');
 
 # Show all things in the inventory table, without their actual tax
 # information
--- db/YardSale_DB/INV_Table.sql
+++ db/YardSale_DB/INV_Table.sql
@@ -10,8 +10,8 @@ CREATE TABLE Inventory_Table(
 	INV_Reorder_Level		INT,
 	INV_Reorder_Quantity		INT,
 	INV_Item_Type			varchar(20),
-	INV_REF_TAX_Tax_Type		INT REFERENCES Tax_Table.TAX_ID,
-	INV_REF_VND_Vendor_ID		INT REFERENCES Vendor_Table.VND_ID,
+	INV_REF_TAX_Tax_Type            INT NOT NULL,
+	INV_REF_VND_Vendor_ID           INT NOT NULL,	
 	INV_Retail_Price		DECIMAL(7,2),
 	INV_Wholesale_Price		DECIMAL(7,2),
 	INV_Bulk_Price			TEXT,
@@ -19,6 +19,13 @@ CREATE TABLE Inventory_Table(
 	INV_Weight_Pounds		FLOAT,
 	INV_Oversized_Flag		enum('T','F'),
 	INV_Ship_By_Freight		enum('T','F'),
+	INV_Comment			TEXT,
 	Primary Key (INV_SKU_Number),
-	UNIQUE INDEX (INV_Bar_Code_Number)
+	UNIQUE INDEX (INV_Bar_Code_Number),
+	INDEX tax_id (INV_REF_TAX_Tax_Type),
+	INDEX vnd_id (INV_REF_VND_Vendor_ID),
+	FOREIGN KEY (INV_REF_TAX_Tax_Type)
+	  REFERENCES Tax_Table(TAX_ID),
+	FOREIGN KEY (INV_REF_VND_Vendor_ID) 
+	  REFERENCES Vendor_Table(VND_ID)
 ) type=InnoDB
