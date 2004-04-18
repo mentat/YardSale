e7d512ac10425efae45f0858448936b50512be86
mrswigon <mrswigon>
2004-04-18 22:10:08 +0000

    maybe it will work this time.

--- src/ys_carrier_type.cpp
+++ src/ys_carrier_type.cpp
@@ -15,7 +15,7 @@ string YardCarrierType::ToString(const string& delim, bool quotes) const {
  
 }
 
-#if (defined(YS_TEST_MAIN) || defined(YS_TEST_CARRIER_TYPE))
+#if (defined(YS_TEST_MAIN) || defined(YS_TEST_CARR_TYPE))
 #include <iostream>
 #include <vector>
 #include "ys_test.h"
@@ -24,21 +24,38 @@ using namespace std;
 
 int main()
 {
-    YardCarrierType test1;
-    test1.SetCarrierName("Big A");
-    test1.SetPickupLoc("mommas place");
-    test1.SetPhoneNum("555-1212");
 
+    YardCarrierType test1;
+    
+    test1.SetCarrierName("BigMike");
+    VERIFY(test1.GetCarrierName(), string("BigMike"));
+    
+    test1.SetPickupLoc("Foxxxy Lady");
+    VERIFY(test1.GetPickupLoc(), string("Foxxxy Lady"));
+    
+    test1.SetPhoneNum("999 123 4567");
+    VERIFY(test1.GetPhoneNum(), string("999 123 4567"))
+ 
+#if 0 
+    vector<XMLNode> v = test1.children();
+   
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i].name() << endl;
+    }
+    
+    cout << string(test1) << endl;
+#endif
     YardCarrierType test2(test1);
     
-    VERIFY(test2.GetCarrierID(), test1.GetCarrierID());
+    VERIFY(test2.GetCarrierName(), test1.GetCarrierName());
     
     YardCarrierType test3;
     
     test3 = test2;
     
-    VERIFY(test3.GetCarrierName(), test2.GetCarrierName());
     VERIFY(test3.GetPickupLoc(), test2.GetPickupLoc());
+    VERIFY(test3.GetPhoneNum(), test2.GetPhoneNum());
 
 
     vector<YardCarrierType> anArray;
@@ -47,14 +64,15 @@ int main()
     anArray.push_back(test2);
     anArray.push_back(test3);
     
-    VERIFY(anArray[1].GetCarrierID(), anArray[2].GetCarrierID());
+    VERIFY(anArray[1].GetPhoneNum(), anArray[2].GetPhoneNum());
     
     vector<YardCarrierType> copyArray;
     copyArray = anArray;
     
-    VERIFY(anArray[0].GetCarrierID(), copyArray[0].GetCarrierID());
-
+    VERIFY(anArray[0].GetPhoneNum(), copyArray[0].GetPhoneNum());
+      
     return failure;
+
 }
 
 #endif
