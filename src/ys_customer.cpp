f3b654a158a7e544f7101c75209a15c3abe59a2e
Jesse Lovelace <jesse.lovelace@gmail.com>
2004-04-20 21:51:06 +0000

    Adding reports.

---
+++ src/ys_customer.cpp
@@ -0,0 +1,204 @@
+#include "extra/xrc/xmlres.h"
+#include "wx/app.h"
+#include "wx/listctrl.h"
+#include "wx/treectrl.h"
+#include "wx/sizer.h"
+#include "wx/log.h"
+#include "wx/stattext.h"
+
+#include "ys_bitmaps.h"
+#include "ys_customer.h"
+#include "ys_exception.h"
+#include "ys_database.h"
+#include "ys_new_cust.h"
+#include "yardsale.h"
+
+#include "images/worker_16x16.xpm"
+
+DECLARE_APP(YardSale)
+
+class custItemData: public wxTreeItemData
+{
+ public:
+    custItemData(long id): 
+        wxTreeItemData(), m_id(id) {}
+	~custItemData() {}
+	
+	const long GetID() { return m_id; }
+
+ private:
+	long m_id;	
+};
+
+BEGIN_EVENT_TABLE(YardCustomer, wxDialog)
+    EVT_TREE_SEL_CHANGED(XRCID("ID_CUST_TREE"), YardCustomer::OnChange)
+    EVT_BUTTON(XRCID("ID_CUST_NEW"), YardCustomer::OnNew)
+END_EVENT_TABLE()
+
+YardCustomer::YardCustomer(wxWindow* parent, wxWindowID id, 
+    const wxString& title, const wxPoint& pos,
+    const wxSize& size, long style, const wxString& name)
+:wxDialog(parent, id, title, pos, size, style, name) {
+    
+    wxXmlResource::Get()->Load("res/customer.xrc");
+    wxPanel * panel = wxXmlResource::Get()->LoadPanel(this, "Customer");
+    wxSizer * sizer = panel->GetSizer();
+    sizer->SetSizeHints(this);
+    SetSize(sizer->GetMinSize());  
+    Centre();
+    
+    m_pic = static_cast<wxStaticBitmap *>(FindWindow(XRCID("ID_CUST_PICTURE")));
+    m_sig = static_cast<wxStaticBitmap *>(FindWindow(XRCID("ID_CUST_SIG")));
+    m_tree = static_cast<wxTreeCtrl *>(FindWindow(XRCID("ID_CUST_TREE")));
+    m_first = static_cast<wxTextCtrl *>(FindWindow(XRCID("ID_CUST_FIRST")));
+    m_middle = static_cast<wxTextCtrl *>(FindWindow(XRCID("ID_CUST_MIDDLE")));
+    m_last = static_cast<wxTextCtrl *>(FindWindow(XRCID("ID_CUST_LAST")));
+    m_address = static_cast<wxTextCtrl *>(FindWindow(XRCID("ID_CUST_ADDRESS")));
+    m_ccNum = static_cast<wxTextCtrl *>(FindWindow(XRCID("ID_CUST_CC")));
+    m_ccName = static_cast<wxTextCtrl *>(FindWindow(XRCID("ID_CUST_CC_NAME")));
+    m_ccExp = static_cast<wxTextCtrl *>(FindWindow(XRCID("ID_CUST_CC_EXP")));
+    m_phone = static_cast<wxTextCtrl *>(FindWindow(XRCID("ID_CUST_PHONE")));
+    m_custSince = static_cast<wxStaticText *>(FindWindow(XRCID("ID_CUST_SINCE")));
+    
+    CreateImageList(m_tree);
+    LoadTreeItems(m_tree);
+    //ID_CUST_PICTURE
+    
+}
+
+void YardCustomer::LoadTreeItems(wxTreeCtrl * tree)
+{
+    tree->SetWindowStyleFlag(wxTR_NO_LINES | wxTR_HIDE_ROOT | wxTR_FULL_ROW_HIGHLIGHT);
+    tree->AddRoot(wxT("Root"));
+    tree->SetIndent(10);
+	tree->SetSpacing(3);
+    
+    vector<YardCustType> customers;
+    
+    try {
+        customers = wxGetApp().DB().CustomerGetAll();
+    }
+    catch (YardDBException& e)
+    {
+        wxLogDebug(wxT("Error (cust load): %s, %s"),e.what(), e.GetSQL().c_str());
+        return;
+    }
+    wxLogDebug(wxT("Got %d customers"), customers.size());
+    for (unsigned int i = 0; i < customers.size(); i++)
+    {
+        wxTreeItemId groupid = tree->AppendItem(tree->GetRootItem(),
+            customers[i].GetFirstLast().c_str(), 0,0,
+            new custItemData(customers[i].GetAccountNumber()));
+    }
+    
+}
+    
+YardCustomer::~YardCustomer()
+{
+   
+
+}
+
+void YardCustomer::CreateImageList(wxTreeCtrl * tree)
+{
+ 	// Make an image list containing small icons
+    wxImageList *images = new wxImageList(16, 16, true);
+
+    wxIcon icons[1];
+	
+    icons[0] = wxIcon(worker_16x16_xpm);
+	
+    for ( size_t i = 0; i < WXSIZEOF(icons); i++ )
+    { 
+		images->Add(wxBitmap(icons[i]));
+    }
+
+    tree->AssignImageList(images);
+}
+
+void YardCustomer::OnChange(wxTreeEvent& event)
+{
+    custItemData * data = static_cast<custItemData *>(m_tree->GetItemData(event.GetItem()));
+
+    if (!data)
+        return;
+    
+    try {
+        m_cust = wxGetApp().DB().CustomerGet(data->GetID());
+    }
+    catch (YardDBException& e)
+    {
+        wxLogDebug(wxT("Error (customer not loaded): %s, %s"),e.what(), e.GetSQL().c_str());
+        return;
+    }
+        
+    if (m_cust.GetPicLocal() != "")
+    {
+        wxImage pic(m_cust.GetPicLocal().c_str());
+        if (pic.Ok())
+            m_pic->SetBitmap(pic);
+        else
+            wxLogDebug(wxT("Bad image data (pic)."));
+    }
+    else
+        m_pic->SetBitmap(wxImage("res/ys_employee_128x128.png"));
+    
+    if (m_cust.GetSigLocal() != "")
+    {
+        wxImage sig(m_cust.GetSigLocal().c_str());
+        if (sig.Ok())
+            m_sig->SetBitmap(sig);
+        else
+            wxLogDebug(wxT("Bad image data (sig)."));
+    }
+    else
+        m_sig->SetBitmap(wxImage("res/empty_200x50.png"));
+    
+    m_first->SetValue(m_cust.GetFirstName().c_str());
+    m_middle->SetValue(m_cust.GetMiddleName().c_str());
+    m_last->SetValue(m_cust.GetLastName().c_str());
+    m_address->SetValue(m_cust.GetAddress().c_str());
+    m_ccNum->SetValue(m_cust.GetCreditCardNumber().c_str());
+    m_ccName->SetValue(m_cust.GetCreditCardName().c_str());
+    m_ccExp->SetValue(m_cust.GetCreditCardExpiration().c_str());
+    m_phone->SetValue(m_cust.GetPhone().c_str());
+    wxString pos;
+    pos.Printf(wxT("Customer Since: %s"), "FIXME");
+    m_custSince->SetLabel(pos);
+}
+
+void YardCustomer::OnNew(wxCommandEvent& event)
+{
+    wxBitmap logo("res/yast_sysadmin.png", wxBITMAP_TYPE_PNG);
+    YardNewCustomer * wizard = new YardNewCustomer(this, -1, 
+        wxT("New Customer Wizard"), logo);
+    
+    YardNewCustomer1 * page1 = new YardNewCustomer1(wizard);
+    YardNewCustomer2 * page2 = new YardNewCustomer2(wizard);
+    YardNewCustomer3 * page3 = new YardNewCustomer3(wizard);
+    YardNewCustomer4 * page4 = new YardNewCustomer4(wizard);
+    
+    wxWizardPageSimple::Chain(page1, page2);
+    wxWizardPageSimple::Chain(page2, page3);
+    wxWizardPageSimple::Chain(page3, page4);
+    
+    wxSize min = page4->GetMin();
+    wizard->SetPageSize(min);
+
+    if ( wizard->RunWizard(page1) )
+    {
+        wxLogDebug(wxT("Wizard completed OK"));
+        
+        YardCustType temp = wizard->GetCustomer();
+        
+        try {
+            wxGetApp().DB().CustomerAdd(temp);
+        }
+        catch (YardDBException& e)
+        {
+            wxLogDebug(wxT("Error (customer not added): %s, %s"),e.what(), e.GetSQL().c_str());
+        }
+    }
+    wizard->Destroy();
+    
+}
---
+++ src/ys_reports.cpp
@@ -0,0 +1,251 @@
+#include <string>
+
+#include "extra/xrc/xmlres.h"
+#include "wx/wx.h"
+#include "wx/app.h"
+#include "wx/calctrl.h"
+#include "wx/html/htmlwin.h"
+
+#include "yardsale.h"
+#include "ys_database.h"
+#include "ys_date.h"
+#include "ys_reports.h"
+#include "sablot.h"
+
+DECLARE_APP(YardSale)
+
+using namespace std;
+
+class YardHTML: public wxDialog
+{
+ public:
+    YardHTML(wxWindow* parent, wxWindowID id, const wxString& title,
+               const wxPoint& pos = wxDefaultPosition, 
+               const wxSize& size = wxDefaultSize,
+               long style = wxDEFAULT_FRAME_STYLE,
+               const wxString& name = wxT("YardHTML"))
+    :wxDialog(parent, id, title, pos, size, style, name)
+    {
+        wxPanel * panel = new wxPanel(this);
+        m_html = new wxHtmlWindow(panel, -1, wxDefaultPosition, wxSize(400,300));
+        
+        wxFlexGridSizer *item0 = new wxFlexGridSizer( 1, 0, 0 );
+        item0->AddGrowableCol( 0 );
+        item0->AddGrowableRow( 0 );
+        item0->Add( m_html, 0, wxGROW|wxALIGN_CENTER_VERTICAL, 5 );
+        panel->SetAutoLayout( TRUE );
+        panel->SetSizer( item0 );
+        
+        item0->SetSizeHints(this);
+        SetSize(item0->GetMinSize());   
+        Centre();
+        
+    }
+    
+    void SetPage(const wxString& html) { m_html->SetPage(html); }
+    
+   
+ private:
+    wxHtmlWindow * m_html;     
+    
+};
+
+
+
+BEGIN_EVENT_TABLE(YardReports, wxDialog)
+    EVT_BUTTON(XRCID("ID_REPORT_EMP_HOURS"), YardReports::OnReportEmpHours)
+    EVT_BUTTON(XRCID("ID_REPORT_EMP_SALES"), YardReports::OnReportEmpSales)
+    EVT_BUTTON(XRCID("ID_REPORT_HOURLY_REV"), YardReports::OnReportHourlyRev)
+END_EVENT_TABLE()
+
+YardReports::YardReports(wxWindow* parent, wxWindowID id, const wxString& title,
+               const wxPoint& pos,const wxSize& size, long style,
+               const wxString& name): wxDialog(parent, id, title, pos, size, style, name)
+{
+    wxXmlResource::Get()->Load("res/reports.xrc");
+    wxPanel * panel = wxXmlResource::Get()->LoadPanel(this, "Reports");
+    wxSizer * sizer = panel->GetSizer();
+    sizer->SetSizeHints(this);
+    SetSize(sizer->GetMinSize());   
+    Centre();
+    
+    m_to = static_cast<wxCalendarCtrl*>(FindWindow(XRCID("ID_REPORT_TO")));
+    m_to->SetHighlightColours(*wxRED, *wxBLACK); 
+    m_from = static_cast<wxCalendarCtrl*>(FindWindow(XRCID("ID_REPORT_FROM")));
+    m_from->SetHighlightColours(*wxRED, *wxBLACK); 
+}
+
+YardReports::~YardReports() {}
+    
+void YardReports::OnReportEmpHours(wxCommandEvent& event)
+{
+    YardDate to(m_to->GetDate());
+    YardDate from(m_from->GetDate());
+    
+    stringstream sql;
+    sql << "SELECT EMP_ID_Number AS ID_Number , EMP_First_Name AS First_Name, Emp_Last_Name AS Last_Name, "
+            "EXTRACT(HOUR FROM LOG_Time_Out - LOG_Time_In) AS Time_Worked_Hours, "
+            "EXTRACT(HOUR_MINUTE FROM LOG_Time_Out - LOG_Time_In)%100 AS Time_Worked_Minutes, "
+            "DATE_FORMAT(Log_Time_Out, '%W %M %D' ) AS Worked_On_Date "
+            "FROM Login_Table JOIN Employee_Table ON LOG_REF_EMP_ID = EMP_ID_Number "
+            "WHERE LOG_Count = 0 AND "
+            "LOG_Time_In BETWEEN '" << from.DayString() << "' AND '" 
+            << to.DayString() << "'; ";
+    
+    string xml("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                "<root>\n");
+    
+    long count = 0;
+    try
+    {
+        xml += wxGetApp().DB().ReportXML(sql.str(), count);
+    }
+    catch (YardDBException& e)
+    {
+        wxLogDebug(wxT("Error (report): %s, %s"),e.what(), e.GetSQL().c_str());
+        return;
+    }
+    wxLogDebug(wxT("Count: %d"), count);
+    
+    xml += "\n</root>\n";
+    
+    string html = ProcessXSLT(xml, "res/report.xsl");
+    
+    YardHTML * prev = new YardHTML(this, -1, wxT("Report Preview"));
+    prev->SetPage(html.c_str());
+    prev->ShowModal();
+    prev->Destroy();
+}
+void YardReports::OnReportEmpSales(wxCommandEvent& event)
+{
+    
+    YardDate to(m_to->GetDate());
+    YardDate from(m_from->GetDate());
+    
+    stringstream sql;
+    sql << "SELECT TRANS_REF_EMP_ID_Number AS Employee_ID, "
+           "EMP_First_Name AS First_Name, EMP_Last_Name AS Last_Name, "
+           "SUM((TRANS_Quantity * (TRANS_Sale_Price - INV_Wholesale_Price))) AS PROFIT, "
+           "SUM(TRANS_Sale_Price * TRANS_Quantity) AS RETAIL_TOTAL, "
+           "SUM(INV_Wholesale_Price * TRANS_Quantity) AS TOTAL_WHOLESALE_COST "
+           "FROM Transaction_Log_Table JOIN Employee_Table "
+           "ON Transaction_Log_Table.TRANS_REF_EMP_ID_Number = Employee_Table.EMP_ID_Number  JOIN Customer_Table "
+           "ON Transaction_Log_Table.TRANS_REF_CUST_Account_Number = Customer_Table.Cust_Account_Number JOIN Inventory_Table "
+           "ON Transaction_Log_Table.TRANS_REF_INV_Item_ID = Inventory_Table.INV_Item_ID "
+           "WHERE "
+           "TRANS_Time BETWEEN '"
+           << from.ToString() << "' AND '" << to.ToString() << "' "
+           "GROUP BY TRANS_REF_EMP_ID_Number;";
+    
+    string xml("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                "<root>\n");
+    
+    long count = 0;
+    try
+    {
+        xml += wxGetApp().DB().ReportXML(sql.str(), count);
+    }
+    catch (YardDBException& e)
+    {
+        wxLogDebug(wxT("Error (report): %s, %s"),e.what(), e.GetSQL().c_str());
+        return;
+    }
+    wxLogDebug(wxT("Count: %d"), count);
+    
+    xml += "\n</root>\n";
+    
+    string html = ProcessXSLT(xml, "res/report_employee_sales.xsl");
+    
+    YardHTML * prev = new YardHTML(this, -1, wxT("Report Preview"));
+    prev->SetPage(html.c_str());
+    prev->ShowModal();
+    prev->Destroy();
+    
+}
+
+void YardReports::OnReportHourlyRev(wxCommandEvent& event)
+{
+
+   YardDate to(m_to->GetDate());
+    YardDate from(m_from->GetDate());
+    
+    stringstream sql;
+    sql << "SELECT EXTRACT(HOUR FROM TRANS_Time) AS Hour, "
+           "SUM((TRANS_Quantity * (TRANS_Sale_Price - INV_Wholesale_Price))) AS PROFIT "
+           "FROM Transaction_Log_Table  JOIN Inventory_Table "
+           "ON Transaction_Log_Table.TRANS_REF_INV_Item_ID = Inventory_Table.INV_Item_ID "
+           "WHERE "
+           "TRANS_Time BETWEEN '"
+           << from.ToString() << "' AND '" << to.ToString() << "' "
+           "GROUP BY EXTRACT(HOUR FROM TRANS_Time) "
+           "UNION "
+           "SELECT EXTRACT(HOUR FROM TRANS_Time)  AS Hour,  "
+           "SUM((TRANS_Quantity * (TRANS_Sale_Price - INV_Wholesale_Price))) AS PROFIT "
+           "FROM Transaction_Log_Table  JOIN Inventory_Table "
+           "ON Transaction_Log_Table.TRANS_REF_INV_Item_ID = Inventory_Table.INV_Item_ID "
+           "WHERE "
+           "TRANS_Time BETWEEN '"
+           << from.ToString() << "' AND '" << to.ToString() << "' "
+           "GROUP BY TRANS_Always_Null;";
+    
+    string xml("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                "<root>\n");
+    
+    long count = 0;
+    try
+    {
+        xml += wxGetApp().DB().ReportXML(sql.str(), count);
+    }
+    catch (YardDBException& e)
+    {
+        wxLogDebug(wxT("Error (report): %s, %s"),e.what(), e.GetSQL().c_str());
+        return;
+    }
+    wxLogDebug(wxT("Count: %d"), count);
+    
+    xml += "\n</root>\n";
+    
+    string html = ProcessXSLT(xml, "res/report_hourly_profit.xsl");
+    
+    YardHTML * prev = new YardHTML(this, -1, wxT("Report Preview"));
+    prev->SetPage(html.c_str());
+    prev->ShowModal();
+    prev->Destroy();    
+    
+    
+}    
+    
+string YardReports::ProcessXSLT(const string& xml_str, const string& xsl_file)
+{
+    string ret;
+    try {
+        SablotSituation S;
+        SablotHandle proc; 
+        SDOM_Document xml;
+    
+        SablotCreateSituation(&S);
+       // SablotParseStylesheetBuffer(S, xslt_str.str().c_str(), &xsl);
+        SablotParseBuffer(S, xml_str.c_str(), &xml);
+    
+        SablotCreateProcessorForSituation(S, &proc);
+       // SablotAddArgTree(S, proc, "sheet", xsl);
+        SablotAddArgTree(S, proc, "data", xml);
+    
+        SablotRunProcessorGen(S, proc, xsl_file.c_str(), "arg:/data", "arg:/out");
+    
+        char * result;
+        SablotGetResultArg(proc, "arg:/out", &result);
+        ret = result;
+            
+        SablotFree(result);
+        SablotDestroyDocument(S, xml);
+        SablotDestroyProcessor(proc);
+        SablotDestroySituation(S);
+    }
+    catch (...)
+    {
+        wxLogDebug("Sablot exception...");
+    }
+    
+    return ret;
+}
